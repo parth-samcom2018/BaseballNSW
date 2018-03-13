2673b88a364d68265e57c29fbd9f3e13ef6bc619
Harshal Choksi <Samcom@I.local>
2018-03-13 16:43:16 +0530

    Baseball

---
+++ app/src/main/java/com/nsw/baseballnsw/models/Register.java
@@ -0,0 +1,17 @@
+package com.nsw.baseballnsw.models;
+
+public class Register {
+    public String email;
+    public String password;
+    public String confirmPassword;
+    public String applicationId;
+    public String firstname;
+    public String surname;
+    public String gender;
+    public String country;
+    public String postCode;
+    public String birthYear;
+
+    public String GroupName;
+
+}
